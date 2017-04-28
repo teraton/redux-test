feea93e181695e627c2afe384ad6189e5d226182
Henri Terho <henri.terho@gmail.com>
2017-04-28 16:26:57 +0300

    Cleaned up the main file and added comments.

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,30 @@
+// A dummy react-redux project to test architectural concepts and different libraries
+
 
 //ie support for promises
 //import { Promise } from 'es6-pro
 import "es6-promise/auto";
 
+//react ui libs
 import * as ReactDOM from 'react-dom'
 import * as React from 'react'
 
+//redux modules for data storage. Also includes state persistence and logging
 import * as Redux from 'redux'
 import * as Actions from './actions'
-
 import {persistStore, autoRehydrate} from 'redux-persist'
-import {Counter} from './components/counter'
 import { Provider } from 'react-redux'
 import { createLogger } from 'redux-logger'
 
+//project components
+import {Counter} from './components/counter'
 import {
     reducers,
     Store
 } from './reducers'
 
-console.log("test1");
-
+//create a logger for the redux store and create the store. Inject middleware and autorehydration
 const logger = createLogger({});
-
 let store: Redux.Store<any> = Redux.createStore (
   reducers,
   undefined,
@@ -32,6 +34,8 @@ let store: Redux.Store<any> = Redux.createStore (
   )
   );
 
+
+//Crosstab sync code, this is acually redux-persist-crosstab code extracted to here
 const KEY_PREFIX = 'reduxPersist:';
 const REHYDRATE = 'persist/REHYDRATE';
 
@@ -45,7 +49,7 @@ function crosstabSync(persistor, config){
   console.log("crosstabSync binding done")
   
   function handleStorageEvent(e){
-    console.log("bound to event")
+    console.log("localStorage change event fired")
     if(e.key.indexOf(KEY_PREFIX) === 0){
       var keyspace = e.key.substr(KEY_PREFIX.length)
       if(whitelist && whitelist.indexOf(keyspace) === -1){ return }
@@ -58,17 +62,17 @@ function crosstabSync(persistor, config){
   }
 }
 
+
+//tie the local storage persist store to the state of the actual store object and enable crosstab sync
 const persistor = persistStore(store, {})
 crosstabSync(persistor, null)
 
-window.localStorage.setItem("foo","bar")
-
+//enable console logging
 store.subscribe(() => {
   console.log(store.getState())
 })
 
-console.log("test");
-// Commented out ("let HTML app be HTML app!")
+//Replace the dummy content in index.html with react content
 window.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('redux-app-root')
   if (rootEl) ReactDOM.render(
