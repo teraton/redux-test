c540e3f1fc2ee259961b3e26f5dce431a047e92a
Henri Terho <henri.terho@gmail.com>
2017-05-03 13:05:41 +0300

    Added comments to the component class

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -11,24 +11,21 @@ import {
 
 import { Store } from '../reducers'
 
+
+//"immutable props that relate to how the component is rendered. For example, label here is not meant to be mutated, it is assigned at render time"
 interface OwnProps {
     label: string;
 }
 
+//the next four segments are explained pretyt well by http://redux.js.org/docs/basics/UsageWithReact.html
+
+//state that is modified from "outside"
 interface ConnectedState  {
   counter: { value: number }
   isSaving: boolean,
   isLoading: boolean,
   error: string,
 }
-
-type ConnectedDispatch = {
-  increment: (n: number) => void
-  save: (n: number) => void
-  load: () => void
-  reset: () => void
-}
-
 const mapStateToProps = (state: Store.All, ownProps: OwnProps): ConnectedState => ({
   counter: state.counter,
   isSaving: state.isSaving,
@@ -36,6 +33,14 @@ const mapStateToProps = (state: Store.All, ownProps: OwnProps): ConnectedState =
   error: state.error,
 })
 
+//these are the props that trigger events. This maps all the changes to action creators?
+
+type ConnectedDispatch = {
+  increment: (n: number) => void
+  save: (n: number) => void
+  load: () => void
+  reset: () => void
+}
 const mapDispatchToProps = (dispatch: redux.Dispatch<Store.All>): ConnectedDispatch => ({
   increment: (n: number) =>
     dispatch(incrementCounter(n)),
@@ -48,12 +53,12 @@ const mapDispatchToProps = (dispatch: redux.Dispatch<Store.All>): ConnectedDispa
 })
 
 
-
+//the own state of the react component. when using redux properly, this should stay empty. All state is stored centrally.
 interface OwnState {};
 
 //-------------------------------------------
 
-//counter component definition
+//counter component definition & event handlers that pass the change events to props, which pass it to action creators through dispatch
 class CounterComponent extends React.Component<OwnProps & ConnectedState & ConnectedDispatch,OwnState>{
      _onClickIncrement = (delta) => {
         this.props.increment(delta);
@@ -89,6 +94,6 @@ class CounterComponent extends React.Component<OwnProps & ConnectedState & Conne
   }
 }
 
-
+//the component is connected to the store here , this is in the bottom because of a bug: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/8787
 export const Counter: React.ComponentClass<OwnProps> =
   connect(mapStateToProps, mapDispatchToProps)(CounterComponent)
